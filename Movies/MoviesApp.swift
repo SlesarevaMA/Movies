448b14e9493ebf3a0b1c6b2dafc76684dcc6a4ec
Margarita Slesareva <slesareva.m.a@yandex.ru>
2023-06-06 22:28:44 +0300

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Margarita Slesareva on 01.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
